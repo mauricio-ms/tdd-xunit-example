7997d30daf90c56c85864edb07fce379d0658411
Maurício Mussatto Scopel <ms.mauricio93@gmail.com>
2021-04-26 23:01:46 -0300

    Chapter 20.

--- WasRun.py
+++ WasRun.py
@@ -3,8 +3,11 @@ from TestCase import TestCase
 
 class WasRun(TestCase):
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun = 1
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = self.log + "tearDown "
+
